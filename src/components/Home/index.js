6f46cbbb5c3de7e42b9ecb596a14f79b350ce978
ferraripuppi <habin@hotmail.co.uk>
2020-03-18 22:26:31 +0000

    home component image + toggle

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,7 +3,16 @@ import * as S from './styles';
 // import { string } from 'prop-types';
 
 const Home = () => {
-  return <S.Wrapper>Home</S.Wrapper>;
+  return (
+    <S.Wrapper>
+      <S.LeftCol>Browse the c0de</S.LeftCol>
+      <S.Container />
+      <S.RightCol>
+        <div>Icons</div>
+        <div>Arrow</div>
+      </S.RightCol>
+    </S.Wrapper>
+  );
 };
 
 export default Home;
--- src/components/Home/styles.js
+++ src/components/Home/styles.js
@@ -1,3 +1,39 @@
 import styled from 'styled-components';
+import homeImg from './assets/home.png';
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100vh;
+  margin: 35px;
+`;
+
+export const LeftCol = styled.div`
+  padding-top: 15px;
+  color: #01bf1b;
+  // font-family: Avenir;
+  font-size: 12px;
+  font-weight: 900;
+  letter-spacing: 0.24px;
+  line-height: 26px;
+  min-width: 200px;
+  width: 10%;
+`;
+
+export const Container = styled.div`
+  background: url(${homeImg});
+  background-size: contain;
+  background-repeat: no-repeat;
+  width: 80%;
+  height: 780px;
+`;
+
+export const RightCol = styled.div`
+  padding-top: 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  min-width: 200px;
+  text-align: center;
+  width: 20%;
+`;
