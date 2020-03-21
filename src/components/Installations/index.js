9f0994d927104c6f2246c6e855d66bbf0719c057
ferraripuppi <habin@hotmail.co.uk>
2020-03-21 19:59:54 +0000

    sets up installations page

--- src/components/Installations/index.js
+++ src/components/Installations/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import * as S from './styles';
+import IconCol from '../IconCol';
 // import { string } from 'prop-types';
 
 const Installations = () => {
-  return <S.Wrapper>Installations</S.Wrapper>;
+  return (
+    <S.Wrapper>
+      <S.TitleCol>
+        <S.Title>Installations</S.Title>
+      </S.TitleCol>
+      <S.Container>Content content content</S.Container>
+      <IconCol />
+    </S.Wrapper>
+  );
 };
 
 export default Installations;
--- src/components/Installations/styles.js
+++ src/components/Installations/styles.js
@@ -1,3 +1,36 @@
 import styled from 'styled-components';
+import installationsImg from './assets/installations.png';
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100vh;
+  margin: 35px;
+  padding-top: 30px;
+`;
+
+export const TitleCol = styled.div`
+  padding-top: 15px;
+  color: #01bf1b;
+  font-size: 12px;
+  font-weight: 600;
+  letter-spacing: 0.24px;
+  line-height: 26px;
+  min-width: 200px;
+  width: 20%;
+`;
+
+export const Title = styled.a`
+  text-decoration: none;
+  color: #d700d4;
+`;
+
+export const Container = styled.div`
+  background: url(${installationsImg});
+  background-size: contain;
+  background-repeat: no-repeat;
+  width: 80%;
+  height: 780px;
+  display: flex;
+  align-items: center;
+`;
